2933067707cd56d5434d79cdcfc87d949676c5f7
AhmedHossam1691977 <ahmedhossam1691977@gmail.com>
2023-07-15 14:00:04 +0300

    First commit

--- js/main.js
+++ js/main.js
@@ -1,27 +1,28 @@
-const Quotes = [
+
+const arrayOfQuotes = [
     {'author': 'Jim Rohn', 
-    'quote': 'Beware of what you become in pursuit of what you want.'
+     'quote': 'Beware of what you become in pursuit of what you want.'
     },
     {'author': 'Epictetus', 
-    'quote': 'It\'s not what happens to you, but how you react to it that matters.'
+     'quote': 'It\'s not what happens to you, but how you react to it that matters.'
     },
     {'author': 'Frank Sinatra', 
-    'quote': 'The best revenge is massive success.'
+     'quote': 'The best revenge is massive success.'
     },
     {'author': 'Wayne Gretzy', 
-    'quote': 'You miss 100% of the shots you don\'t take.'
+     'quote': 'You miss 100% of the shots you don\'t take.'
     },
     {'author': 'Nelson Mandela', 
-    'quote': 'Resentment is like drinking poison and waiting for your enemies to die.'
+     'quote': 'Resentment is like drinking poison and waiting for your enemies to die.'
     },
     {'author': 'Elbert Hubbard', 
-    'quote': 'Do not take life too seriously. You will not get out alive.'
+     'quote': 'Do not take life too seriously. You will not get out alive.'
     },
 ];
 
 function change(){
-    const random = Number.parseInt(Math.random()*Quotes.length + 1);
-    var quoteOutput =document.getElementById('quoteOutput').innerHTML=Quotes
-    var authorOutput =document.getElementById('authorOutput').innerHTML =Quotes
+    const random = Number.parseInt(Math.random()*arrayOfQuotes.length + 1);
+    document.querySelector('#quoteOutput').textContent = `\"${arrayOfQuotes[random].quote}\"`;
+    document.querySelector('#authorOutput').textContent = `--${arrayOfQuotes[random].author}`;
     
 }
